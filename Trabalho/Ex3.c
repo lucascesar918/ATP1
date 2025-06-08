876da1bd947f39c2bbe0ae27ef9cbcf07314887b
lucascesar918 <lc.freitas@unesp.br>
2025-06-08 10:22:15 -0300

    add Ex3

---
+++ Trabalho/Ex3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void substring(char* src, char* dest, size_t start, size_t end);
+
+int main(){
+  size_t bufsize = 256;
+  char* buf = (char*)malloc(sizeof(char) * bufsize);
+  char* auxbuf = (char*)malloc(sizeof(char) * bufsize);
+
+  fgets(buf, bufsize, stdin);
+  substring(buf, auxbuf, 4, 9);
+  printf("%s\n", auxbuf);
+
+  free(buf);
+  free(auxbuf);
+  return 0;
+}
+
+void substring(char* src, char* dest, size_t start, size_t end){
+  if (start > end) return;
+  size_t c=0, i=start;
+
+  while (i<=end)
+    dest[c++] = src[i++];
+
+  dest[c] = '\0';
+}
